a8674869b49591c2707a3f42c0d93bdd566192be
123minjae <moonlight6750@naver.com>
2024-09-28 21:18:33 +0900

    shap on project
    add shap components to django project

--- prediction/views.py
+++ prediction/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.conf import settings
 from .models_files.saint_model import SAINT
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import shap
 
 # 모델 초기화 및 로드 (서버 시작 시 한 번만 로드)
 MODEL_PATH = os.path.join(settings.BASE_DIR, 'prediction', 'models_files', 'saint_model.pth')
@@ -18,6 +21,37 @@ state_dict = torch.load(MODEL_PATH, map_location=torch.device('cpu'), weights_on
 model.load_state_dict(state_dict)
 model.eval()
 
+EXPLAINER_PATH = os.path.join(settings.BASE_DIR,'prediction','data','The_Cancer_data_1500_V3.csv')
+data = pd.read_csv(EXPLAINER_PATH)
+Y = data['Diagnosis'].values
+X = data[['Age', 'Gender', 'BMI', 'Smoking', 'PhysicalActivity', 'AlcoholIntake', 'CancerHistory']].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# train 데이터 torch 형태로 변환
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+Y_train_tensor = torch.tensor(Y_train, dtype=torch.float32).unsqueeze(1)
+
+# explainer 선언
+explainer = shap.GradientExplainer(SAINT(INPUT_DIM, HIDDEN_DIM, 1), X_train_tensor)
+shap_values_train = explainer.shap_values(X_train_tensor)
+
+# column features 선언
+columns = ['Age', 'Gender', 'BMI', 'Smoking', 'PhysicalActivity', 'AlcoholIntake', 'CancerHistory']
+
+# shap_values_train의 shape이 (샘플 수, feature 수, 1)이므로, 이를 2차원으로 변환
+shap_values_train_2d = np.squeeze(shap_values_train)  # 차원 축소
+shap_mean_abs_values = np.abs(shap_values_train_2d).mean(axis=0) # shap 값 담기
+
+# Feature별 평균 임팩트 값을 DataFrame으로 변환
+shap_mean_abs_df = pd.DataFrame({
+    'Feature': columns,
+    'Mean_SHAP_Value': shap_mean_abs_values
+})
+
+# SHAP 값의 평균 임팩트를 큰 순서대로 정렬하여 출력
+shap_mean_abs_df_sorted = shap_mean_abs_df.sort_values(by='Mean_SHAP_Value', ascending=False)
+
+print(shap_mean_abs_df_sorted)
 
 def preprocess_input(age, gender, bmi, smoking, physical_activity, alcohol_intake, cancer_history):
     
@@ -70,6 +104,19 @@ def predict_cancer(request):
 
         # 암이 있을 확률만 출력
         result = f"암 발병 확률: {cancer_probability * 100:.2f}%"
+        # 입력값 explainer에 학습
+        shap_values_input = explainer.shap_values(input_tensor)
+        shap_values_input_2d = shap_values_input.squeeze(2) # 차원 축소
+        shap_mean_abs_input_values = np.abs(shap_values_input_2d).mean(axis=0)
+
+        # Feature별 평균 임팩트 값을 DataFrame으로 변환
+        shap_mean_abs_input_df = pd.DataFrame({
+            'Feature': columns,
+            'Mean_SHAP_Value': shap_mean_abs_input_values
+        })
+        shap_mean_abs_input_df_sorted = shap_mean_abs_input_df.sort_values(by='Mean_SHAP_Value', ascending=False)
+        # index를 feature로 변경
+        shap_mean_abs_input_df_final = shap_mean_abs_input_df_sorted.set_index('Feature')
 
         return render(request, 'prediction/result.html', {'result': result})
 
